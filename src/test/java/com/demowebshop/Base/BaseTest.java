9b02115174cbcb1593aefd6e419d631f0a7147b1
Automation Tester2 <smbikkina@gmail.com>
2025-12-07 13:51:04 +0530

    dependencies and baseTest Added

---
+++ src/test/java/com/demowebshop/Base/BaseTest.java
@@ -0,0 +1,28 @@
+package com.demowebshop.Base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+	protected WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() {
+		driver = new ChromeDriver();
+		//driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		if(driver != null) {
+			driver.quit();
+		}
+		
+	}
+}
