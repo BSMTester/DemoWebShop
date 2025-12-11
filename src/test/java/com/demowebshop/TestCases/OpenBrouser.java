f8188a570462a548a2810344a743eb80dc4a505f
Automation Tester2 <smbikkina@gmail.com>
2025-12-11 19:02:51 +0530

    added chrome browser test

---
+++ src/test/java/com/demowebshop/TestCases/OpenBrouser.java
@@ -0,0 +1,14 @@
+package com.demowebshop.TestCases;
+
+import org.testng.annotations.Test;
+
+import com.demowebshop.Base.BaseTest;
+
+public class OpenBrouser extends BaseTest{
+	
+	@Test
+	public void openChrome() {
+		driver.get("https://demowebshop.tricentis.com/");
+	}
+
+}
