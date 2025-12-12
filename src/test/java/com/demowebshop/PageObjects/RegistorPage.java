dbe6daa314888f432a1187d61890ab020af55ae1
Automation Tester2 <smbikkina@gmail.com>
2025-12-12 14:01:04 +0530

    DEM-3 modified Registor Page with Constructor

--- src/test/java/com/demowebshop/PageObjects/RegistorPage.java
+++ src/test/java/com/demowebshop/PageObjects/RegistorPage.java
@@ -1,5 +1,15 @@
 package com.demowebshop.PageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class RegistorPage {
+	
+	WebDriver driver;
+	
+	public RegistorPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
 
 }
