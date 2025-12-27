1548e14eb35d02ea51985645fdfa9d5de10be8ce
Tester A <testerbsm123@gmail.com>
2025-12-27 12:34:33 +0530

    Framework: add BasePage for avoid hotcoding

--- src/test/java/com/demowebshop/Base/BaseTest.java
+++ src/test/java/com/demowebshop/Base/BaseTest.java
@@ -4,24 +4,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
-import com.beust.jcommander.Parameter;
+import com.demowebshop.Utilities.Readconfig;
 
 public class BaseTest {
 
-	protected WebDriver driver;
+	public WebDriver driver;
 	
-	@BeforeMethod
+	@BeforeClass
 	public void setup() {
 		
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.get("https://demowebshop.tricentis.com/register");
+		Readconfig readconfig = new Readconfig();
+		driver.get(readconfig.getApplicationUrl());
 	}
 	
-	@AfterMethod
+	@AfterClass
 	public void tearDown() {
 		if(driver != null) {
 			driver.quit();
--- src/test/java/com/demowebshop/TestCases/RegistrationTest.java
+++ src/test/java/com/demowebshop/TestCases/RegistrationTest.java
@@ -1,13 +1,21 @@
 package com.demowebshop.TestCases;
 
 import com.demowebshop.Base.BaseTest;
+import com.demowebshop.PageObjects.BasePage;
 import com.demowebshop.PageObjects.RegistorPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class RegistrationTest extends BaseTest {
 
+	@BeforeMethod
+	public void beforeRegistrationTest() {
+		BasePage bp = new BasePage(driver);
+		bp.clickRegisterLink();
+	}
+	
     @Test(description = "XRAY-DEM-67", priority = 1)
     public void TC_Register_with_valid_data(){
         RegistorPage registorPage = new RegistorPage(driver);
