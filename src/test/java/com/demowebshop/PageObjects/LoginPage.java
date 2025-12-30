bbb38044d051b00727fc07ff245bee3bf5cd0777
Tester B <smbikkina@gmail.com>
2025-12-30 14:39:51 +0530

    Framework:Login-pageObject

---
+++ src/test/java/com/demowebshop/PageObjects/LoginPage.java
@@ -0,0 +1,67 @@
+package com.demowebshop.PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LoginPage {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @FindBy(id="Email")
+    WebElement emailTestBox;
+
+    @FindBy(name = "Password")
+    WebElement passwordTextBox;
+
+    @FindBy(xpath = "//input[@value='Log in']")
+    WebElement LoginButton;
+
+    @FindBy(xpath = "//li[text()='No customer account found']")
+    WebElement LoginError;
+
+    @FindBy(xpath = "//span[@for='Email']")
+    WebElement invalidEmailErr;
+
+    public void enterLoginEmail(String LoginmailId){
+        wait.until(ExpectedConditions.visibilityOf(emailTestBox));
+        emailTestBox.clear();
+        emailTestBox.sendKeys(LoginmailId);
+    }
+
+    public void enterLoginPassword(String LoginPassword){
+        wait.until(ExpectedConditions.visibilityOf(passwordTextBox));
+        passwordTextBox.clear();
+        passwordTextBox.sendKeys(LoginPassword);
+    }
+
+    public void clickLoginButton(){
+        LoginButton.click();
+    }
+
+    public String getLoginErrMsg(){
+        wait.until(ExpectedConditions.elementToBeSelected(LoginError));
+        return LoginError.getText();
+    }
+
+    public String getInvalidMailErrMsg(){
+        wait.until(ExpectedConditions.elementToBeSelected(invalidEmailErr));
+        return invalidEmailErr.getText();
+    }
+
+    public String getCurrentUrl(){
+        return driver.getCurrentUrl();
+    }
+
+}
