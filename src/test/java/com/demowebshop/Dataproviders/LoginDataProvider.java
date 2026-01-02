0d233cca8e9ca6d5b4d0e10fba6cf95ec53a7874
Tester B <smbikkina@gmail.com>
2026-01-02 14:51:28 +0530

    Resolve conflict Login data Provider

--- src/test/java/com/demowebshop/Dataproviders/LoginDataProvider.java
+++ src/test/java/com/demowebshop/Dataproviders/LoginDataProvider.java
@@ -5,19 +5,6 @@ import org.testng.annotations.DataProvider;
 public class LoginDataProvider { 
 			
     @DataProvider (name = "LoginData")
-<<<<<<< HEAD
-    public static Object[][] getLoginData(){
-         String XLPath = System.getProperty("user.dir") + "/src/test/Resources/TestData/Test Data of DemoWebShop.xls";
-        ExcelUtil.setExcelFile(XLPath, "Login Data");
-        int rowCount = ExcelUtil.getRowCount();
-        int colCount = ExcelUtil.getColumnCount();
-        String[][] data = new String[rowCount][colCount];
-        for(int i = 1; i<=rowCount; i++){
-            for(int j=0;j<colCount;j++){
-                data[i-1][j] = ExcelUtil.getExcelData(i, j);
-            }
-        } 
-=======
      public Object[][]  getLoginData() throws Exception{
       String XLPath =  "D:\\Git Projects\\DemoWebShop\\src\\test\\Resources\\TestData\\Test Data of DemoWebShop.xlsx";
     	String XLSheet = "LoginData";
@@ -31,7 +18,6 @@ public class LoginDataProvider {
         // 	System.out.println(data[i-1][j]);
         	}
         }
->>>>>>> 4cf719d2bf9ed07b09d96325dff101b54744939f
         return data;
         }
     }
